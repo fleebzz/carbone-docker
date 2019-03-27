0dbba42439e2ee03c01a3f0a9c242ac83a34ddab
Florian Bezagu <florian@bezagu.com>
2019-03-27 18:19:36 +0100

    First working version

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:8
+
+WORKDIR /tmp
+RUN wget https://downloadarchive.documentfoundation.org/libreoffice/old/5.4.7.2/deb/x86_64/LibreOffice_5.4.7.2_Linux_x86-64_deb.tar.gz -O libo.tar.gz
+RUN apt update \
+  && apt install -y libxinerama1 libfontconfig1 libdbus-glib-1-2 libcairo2 libcups2 libglu1-mesa libsm6 unzip \
+  && tar -zxvf libo.tar.gz
+WORKDIR LibreOffice_5.4.7.2_Linux_x86-64_deb/DEBS
+RUN dpkg -i *.deb
+
+RUN mkdir /tmp-reports
+
+WORKDIR /carbone-api
+
+CMD bash
---
+++ index.js
@@ -0,0 +1,49 @@
+const path = require(`path`);
+const fs = require(`fs-extra`);
+const util = require(`util`);
+const carbone = require(`carbone`);
+const express = require(`express`);
+const bodyParser = require(`body-parser`);
+const app = express();
+const upload = require(`multer`)({ dest: `/tmp-reports/` });
+const port = process.env.CARBONE_PORT || 3030;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const render = util.promisify(carbone.render);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(`./test.html`));
+});
+
+app.post('/render', upload.single(`template`), async (req, res) => {
+  const template = req.file;
+  const originalNameWOExt = template.originalname.split(`.`).slice(0, -1).join(`.`);
+  const originalFormat = template.originalname.split(`.`).reverse()[0];
+  let data = req.body.data;
+  const format = req.body.format || originalFormat;
+  const outputName = req.body.outputName || `${originalNameWOExt}.${format}`;
+  if (typeof data !== `object` || data === null) {
+    try {
+      data = JSON.parse(req.body.data);
+    } catch (e) {
+      data = {};
+    }
+  }
+
+  const options = {
+    convertTo: format,
+  };
+
+  const report = await render(template.path, data, options);
+
+  fs.remove(template.path);
+
+  res.setHeader(`Content-Disposition`, `attachment; filename=${outputName}`);
+  res.setHeader(`Content-Transfer-Encoding`, `binary`);
+	res.setHeader(`Content-Type`, `application/octet-stream`);
+  res.send(report);
+});
+
+app.listen(port, () => console.log(`Carbone wrapper listenning on port ${port}!`));
