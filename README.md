1f9bbc9dc24e48aaa91f6d0298d5e01b9a25cbea
Florian Bezagu <florian@bezagu.com>
2019-10-17 16:09:11 +0200

    Added compatibility with formatters

--- README.md
+++ README.md
@@ -1,9 +1,12 @@
 # Carbone Docker
-Embedded Carbone in a Docker image with simple REST interface.
+Embedded Carbone in a Docker image with simple REST API.
 
-### From carbone.io website
+## How to consume exposed API ?
+The simpliest way to use this image is to use `node` and install [`carbone-connect` package](https://npmjs.org/carbone-connect).
+
+## From carbone.io website
 _Fast, Simple and Powerful report generator in any format PDF, DOCX, XLSX, ODT, PPTX, ODS [, ...]_
 
 _... using your JSON data as input._
 
-See [carbone.io website](https://carbone.io) for documentation on how to use it.
+See [carbone.io website](https://carbone.io) for full **Carbone** documentation.
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const path = require(`path`);
 const fs = require(`fs-extra`);
+const _ = require(`lodash`);
 const util = require(`util`);
 const carbone = require(`carbone`);
+const telejson = require(`telejson`);
 const express = require(`express`);
 const bodyParser = require(`body-parser`);
 const app = express();
@@ -13,6 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 const render = util.promisify(carbone.render);
 
+// Flagging default formatters to remove custom ones later
+_.forEach(carbone.formatters, formatter => formatter.$isDefault = true);
+
 app.get('/', (req, res) => {
   res.sendFile(path.resolve(`./test.html`));
 });
@@ -23,6 +28,7 @@ app.post('/render', upload.single(`template`), async (req, res) => {
   const originalFormat = template.originalname.split(`.`).reverse()[0];
   let data = req.body.data;
   let options = {};
+  let formatters = {};
   try {
     options = JSON.parse(req.body.options);
   } catch (e) {}
@@ -35,15 +41,32 @@ app.post('/render', upload.single(`template`), async (req, res) => {
       data = {};
     }
   }
+  try {
+    formatters = telejson.parse(req.body.formatters);
+  } catch (e) {}
 
-  const report = await render(template.path, data, options);
+  // Removing previous custom formatters before adding new ones
+  carbone.formatters = _.filter(carbone.formatters, formatter => formatter.$isDefault === true);
+
+  carbone.addFormatters(formatters);
+
+  let report = null;
+
+  try {
+    report = await render(template.path, data, options);
+  } catch (e) {
+    console.log(e);
+    return res.status(500).send(`Internal server error`);
+  }
 
   fs.remove(template.path);
 
   res.setHeader(`Content-Disposition`, `attachment; filename=${options.outputName}`);
   res.setHeader(`Content-Transfer-Encoding`, `binary`);
 	res.setHeader(`Content-Type`, `application/octet-stream`);
-  res.send(report);
+  res.setHeader(`Carbone-Report-Name`, options.outputName);
+
+  return res.send(report);
 });
 
 app.listen(port, () => console.log(`Carbone wrapper listenning on port ${port}!`));
